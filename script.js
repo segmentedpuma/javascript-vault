68a9e81e36e999c6d23f03cf93d495e1c2376d91
Gabriel <segmentedpuma@LAPTOP-DKL1MBK8>
2024-03-04 13:40:47 -0600

    oopsies, forgot to save the .js file

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 /* create three variables which are the end calculations of the vault code (10, 40, 39).
- create string which uses the three variables, put the string with the combination in an alert box
+  create string which tells the user about being chosen for the vault, and which
+  uses the three variables, put the string with the combination in an alert box
 */
 
 // creation of varibles whose caluclations equal the code combination (10, 40, 39)
@@ -9,7 +10,12 @@ const num3 = num2 - 1; // subtracted 1 from num2(40), num3 now equals 39
 
 //combination of all three variables into string
 const string = (`You have received this message because you have been chosen to open an important vault.
- Here is the secret combination:${num1} - ${num2} - ${num3}`); //the variable "string" now equals a string with the code combination separated by dashes
+
+Here is the secret combination: ${num1} - ${num2} - ${num3}`);/*the variable "string" now equals a string
+ with the code combination separated by dashes along with the message telling the user about the combination 
+ (there is an extra line to make the alert have a break separatig the message and the combination)
+*/
+ 
 
 //show user dialog box with string containing code combination
 alert(string); //the varibale "string", which contains the code combination, is sent to the user as an alert box.
